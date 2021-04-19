55637edc3f2303dc271ea82a8f10ff586f68fdca
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-04-19 17:34:04 +0200

    copyCustomerToDTO and /customer (saveCustomer)

--- src/main/java/com/udacity/jdnd/course3/critter/controller/UserController.java
+++ src/main/java/com/udacity/jdnd/course3/critter/controller/UserController.java
@@ -3,10 +3,16 @@ package com.udacity.jdnd.course3.critter.controller;
 import com.udacity.jdnd.course3.critter.dto.CustomerDTO;
 import com.udacity.jdnd.course3.critter.dto.EmployeeDTO;
 import com.udacity.jdnd.course3.critter.dto.EmployeeRequestDTO;
+import com.udacity.jdnd.course3.critter.entity.Customer;
+import com.udacity.jdnd.course3.critter.service.PetService;
+import com.udacity.jdnd.course3.critter.service.UserService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.DayOfWeek;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 /**
@@ -19,9 +25,25 @@ import java.util.Set;
 @RequestMapping("/user")
 public class UserController {
 
+    private static final String []  PROPERTIES_TO_IGNORE_ON_COPY = { "id" };
+
+    private UserService userService;
+
+    private PetService petService;
+
+    public UserController(UserService userService, PetService petService) {
+        this.userService = userService;
+        this.petService = petService;
+    }
+
     @PostMapping("/customer")
     public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO){
-        throw new UnsupportedOperationException();
+        Long id = Optional.ofNullable(customerDTO.getId()).orElse(Long.valueOf(-1));
+        Customer c = userService.findCustomer(id).orElseGet(Customer::new);
+        BeanUtils.copyProperties(customerDTO, c, PROPERTIES_TO_IGNORE_ON_COPY);
+        List<Long> petIds = Optional.ofNullable(customerDTO.getPetIds()).orElseGet(ArrayList::new);
+        c = userService.save(c, petIds);
+        return copyCustomerToDTO(c);
     }
 
     @GetMapping("/customer")
@@ -54,4 +76,13 @@ public class UserController {
         throw new UnsupportedOperationException();
     }
 
+
+    private CustomerDTO copyCustomerToDTO(Customer c){
+        CustomerDTO dto = new CustomerDTO();
+        BeanUtils.copyProperties(c, dto);
+        c.getPets().forEach( pet -> {
+            dto.getPetIds().add(pet.getId());
+        });
+        return dto;
+    }
 }
