d888adfc810736d526c6ea94a9f61f8a66129d07
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-04-19 19:49:24 +0200

    savePet and getPet

--- src/main/java/com/udacity/jdnd/course3/critter/controller/PetController.java
+++ src/main/java/com/udacity/jdnd/course3/critter/controller/PetController.java
@@ -1,9 +1,16 @@
 package com.udacity.jdnd.course3.critter.controller;
 
 import com.udacity.jdnd.course3.critter.dto.PetDTO;
+import com.udacity.jdnd.course3.critter.entity.Pet;
+import com.udacity.jdnd.course3.critter.exceptions.PetNotFoundException;
+import com.udacity.jdnd.course3.critter.service.PetService;
+import com.udacity.jdnd.course3.critter.service.UserService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Handles web requests related to Pets.
@@ -11,15 +18,33 @@ import java.util.List;
 @RestController
 @RequestMapping("/pet")
 public class PetController {
+    private static final String []  PROPERTIES_TO_IGNORE_ON_COPY = { "id" };
+
+    @Autowired
+    PetService petService;
+
+    @Autowired
+    UserService userService;
 
     @PostMapping
     public PetDTO savePet(@RequestBody PetDTO petDTO) {
-        throw new UnsupportedOperationException();
+        long petId = Optional.ofNullable(petDTO.getId()).orElse(-1L);
+        Pet pet = petService.findPet(Long.valueOf(petId)).orElseGet(Pet::new);
+        BeanUtils.copyProperties(petDTO, pet, PROPERTIES_TO_IGNORE_ON_COPY);
+        pet = petService.save(pet, petDTO.getOwnerId());
+
+        PetDTO dto = new PetDTO();
+        BeanUtils.copyProperties(pet, dto);
+        dto.setOwnerId(pet.getOwner().getId());
+        return dto;
     }
 
     @GetMapping("/{petId}")
     public PetDTO getPet(@PathVariable long petId) {
-        throw new UnsupportedOperationException();
+        PetDTO petDTO = new PetDTO();
+        Pet pet = petService.findPet(petId).orElseThrow(()->new PetNotFoundException("ID: " + petId));
+        BeanUtils.copyProperties(pet,petDTO);
+        return petDTO;
     }
 
     @GetMapping
