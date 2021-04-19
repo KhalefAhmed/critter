489db6edd2cde2d7e8c23dd1f13cbe0c00b63850
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-04-19 16:44:44 +0200

    findAvailableEmployees

--- src/main/java/com/udacity/jdnd/course3/critter/service/UserService.java
+++ src/main/java/com/udacity/jdnd/course3/critter/service/UserService.java
@@ -2,22 +2,29 @@ package com.udacity.jdnd.course3.critter.service;
 
 import com.udacity.jdnd.course3.critter.entity.Customer;
 import com.udacity.jdnd.course3.critter.entity.Employee;
+import com.udacity.jdnd.course3.critter.entity.EmployeeSkill;
 import com.udacity.jdnd.course3.critter.entity.Pet;
 import com.udacity.jdnd.course3.critter.exceptions.CustomerNotFoundException;
 import com.udacity.jdnd.course3.critter.exceptions.EmployeeNotFoundException;
 import com.udacity.jdnd.course3.critter.exceptions.PetNotFoundException;
 import com.udacity.jdnd.course3.critter.repository.CustomerRepository;
+import com.udacity.jdnd.course3.critter.repository.EmployeeManagedRepository;
 import com.udacity.jdnd.course3.critter.repository.EmployeeRepository;
 import com.udacity.jdnd.course3.critter.repository.PetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class UserService {
+    @Autowired
+    EmployeeManagedRepository employeeManagedRepository;
+
     @Autowired
     CustomerRepository customerRepository;
 
@@ -56,5 +63,13 @@ public class UserService {
 
     public Customer findOwnerByPet(Long id) throws CustomerNotFoundException {
         return customerRepository.findOptionalByPetId(id).orElseThrow(() -> new CustomerNotFoundException("ID: " + id));
+    }
+
+    public List<Employee> findAvailableEmployees(Set<EmployeeSkill> skills, LocalDate date) {
+        // if there is more than one skill Hibernate does not support queries on @EnumeratedCollections
+        // So get the ids of the employees with all skills and then pull just those employees from the database.
+        List<Long> employeesIds = employeeManagedRepository.findEmployeeIdsWithAllSkillsOnDay(skills, date.getDayOfWeek());
+        List<Employee> employees = employeeRepository.findAllById(employeesIds);
+        return employees;
     }
 }
