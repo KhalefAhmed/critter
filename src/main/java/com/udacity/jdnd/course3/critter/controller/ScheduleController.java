0a51293f0d1a6a92ef2ecd4475a97e693946d7b1
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-04-20 12:25:03 +0200

    createSchedule and transform to/from DTO

--- src/main/java/com/udacity/jdnd/course3/critter/controller/ScheduleController.java
+++ src/main/java/com/udacity/jdnd/course3/critter/controller/ScheduleController.java
@@ -1,9 +1,18 @@
 package com.udacity.jdnd.course3.critter.controller;
 
 import com.udacity.jdnd.course3.critter.dto.ScheduleDTO;
+import com.udacity.jdnd.course3.critter.entity.Schedule;
+import com.udacity.jdnd.course3.critter.exceptions.EmployeeNotFoundException;
+import com.udacity.jdnd.course3.critter.exceptions.PetNotFoundException;
+import com.udacity.jdnd.course3.critter.service.PetService;
+import com.udacity.jdnd.course3.critter.service.ScheduleService;
+import com.udacity.jdnd.course3.critter.service.UserService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Handles web requests related to Schedules.
@@ -12,9 +21,30 @@ import java.util.List;
 @RequestMapping("/schedule")
 public class ScheduleController {
 
+    private static final String []  PROPERTIES_TO_IGNORE_ON_COPY = { "id" };
+
+    @Autowired
+    ScheduleService scheduleService;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    PetService petService;
+
+
     @PostMapping
-    public ScheduleDTO createSchedule(@RequestBody ScheduleDTO scheduleDTO) {
-        throw new UnsupportedOperationException();
+    public ScheduleDTO createSchedule(@RequestBody ScheduleDTO scheduleDTO)
+            throws EmployeeNotFoundException, PetNotFoundException {
+        Schedule schedule = scheduleService.findSchedule(scheduleDTO.getId()).orElseGet(Schedule::new);
+        schedule.setDate(scheduleDTO.getDate());
+        schedule.setActivities(scheduleDTO.getActivities());
+        schedule.setEmployees(userService.findEmployees(scheduleDTO.getEmployeeIds()));
+        schedule.setPets(petService.findPets(scheduleDTO.getPetIds()));
+        schedule = scheduleService.save(schedule);
+
+        return copyScheduleToDTO(schedule);
+
     }
 
     @GetMapping
@@ -35,5 +65,20 @@ public class ScheduleController {
     @GetMapping("/customer/{customerId}")
     public List<ScheduleDTO> getScheduleForCustomer(@PathVariable long customerId) {
         throw new UnsupportedOperationException();
+    }
+
+    private List<ScheduleDTO> copySchedulesToDTO(List<Schedule> schedules) {
+        return schedules
+                .stream()
+                .map(s -> { return copyScheduleToDTO(s); })
+                .collect(Collectors.toList());
+    }
+
+    private ScheduleDTO copyScheduleToDTO(Schedule s) {
+        ScheduleDTO dto = new ScheduleDTO();
+        BeanUtils.copyProperties(s, dto);
+        s.getEmployees().forEach(employee -> {dto.getEmployeeIds().add(employee.getId());});
+        s.getPets().forEach(pet -> {dto.getPetIds().add(pet.getId());});
+        return dto;
     }
 }
